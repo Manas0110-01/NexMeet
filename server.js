e16caf0a4ba5cba76e9debb5239a3fa205269047
Manas0110-01 <yadavmanas1217@gmail.com>
2026-09-16 02:15:04 +0530

    Update require paths to match Models folder

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
-const User = require('./models/User');
-const Meeting = require('./models/Meeting');
+const User = require('./Models/user');
+const Meeting = require('./Models/Meeting');
 
 const app = express();
 const server = http.createServer(app);
